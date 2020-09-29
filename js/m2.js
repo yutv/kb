8752dd8f3e78e0bb1d111f6578605094f74b94f8
yutv <yurii.tvardyi@gmail.com>
2020-09-29 22:19:50 +0200

    Deployed 3cb3934 with MkDocs version: 1.0.4

--- js/m2.js
+++ js/m2.js
@@ -1,33 +1,29 @@
-window.uiQuery = (q) => {
-    q = q || '';
-    function execute(q) {
-        require(['uiRegistry'], registry => {
-            window.m2ui.map((name) => {
-                if (!name || !~name.indexOf(q)) return;
-                console.groupCollapsed(name);
-                console.log(registry.get(name));
-                console.groupEnd();
-            });
-        });
-    }
-    if (window.m2ui) {
-        execute(q);
+require(['uiRegistry'], registry => {
+    window.uiq = (q) => {
+        q = q || '';
 
-        return;
-    }
-    require(['uiRegistry'], registry => {
+        var result = [];
         registry.get((component, name) => {
-            window.m2ui = window.m2ui || [];
-            window.m2ui.push(name);
+            if (name && !!~name.indexOf(q)) {
+                result.push(name);
+            }
         });
-        window.m2ui.sort();
-        execute(q);
 
-        return;
-    });
-};
+        if (result.length === 1) {
+            return registry.get(result[0]);
+        }
 
-window.uiInfo = (selector) => {
+        result.sort().forEach(function(name) {
+            var component = registry.get(name);
+            console.groupCollapsed(name);
+            console.log('template', component.template);
+            console.log(component);
+            console.groupEnd();
+        });
+    }
+});
+
+window.uii = (selector) => {
     var node = selector ? document.querySelector(selector) : $0;
     if (!node) {
         console.error('Inspect DOM element or provide a correct selector');
